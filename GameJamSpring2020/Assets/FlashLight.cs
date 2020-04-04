b1dd15be2eba365b44a8c587715a35796ce720b2
Bazzan <sebake01@gmail.com>
2020-04-04 17:27:31 +0200

    FlashLight and sounds

--- GameJamSpring2020/Assets/FlashLight.cs
+++ GameJamSpring2020/Assets/FlashLight.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class FlashLight : MonoBehaviour
 {
 
-
-    Light flashLight;
+    public float battery = 200;
+    private Light flashLight;
+    private bool active = false;
     // Start is called before the first frame update
     void Start()
     {
         flashLight = GetComponent<Light>();
+        flashLight.enabled = false;
     }
 
     // Update is called once per frame
@@ -18,14 +20,36 @@ public class FlashLight : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (flashLight.enabled)
-            {
-                flashLight.enabled = false;
-            }
-            else
-            {
-                flashLight.enabled = true;
+            if(battery > 5) { 
+            
+                if (!active)
+                {
+                    active = true;
+                    flashLight.enabled = true;
+
+                }
+                else
+                {
+                    active = false;
+                    flashLight.enabled = false;
+                }
             }
         }
+
+
+        if (active)
+        {
+            battery -= 10 * Time.deltaTime;
+            Debug.Log(battery);
+            if (battery <= 0)
+            {
+                active = false;
+                flashLight.enabled = false;
+                battery = 0;
+                Debug.Log("no battery");
+
+            }
+        }
+
     }
 }
