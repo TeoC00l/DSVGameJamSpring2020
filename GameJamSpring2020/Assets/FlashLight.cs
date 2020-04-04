0d8f43dc6132c2ac01ced6c5381d69e754b3032e
Bazzan <sebake01@gmail.com>
2020-04-04 14:17:56 +0200

    FlashLight

---
+++ GameJamSpring2020/Assets/FlashLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+
+
+    Light flashLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (flashLight.enabled)
+            {
+                flashLight.enabled = false;
+            }
+            else
+            {
+                flashLight.enabled = true;
+            }
+        }
+    }
+}
