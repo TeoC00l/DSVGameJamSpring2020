a503b5e4b8720ebacde06896a301c5edbd7a8ed5
Bazzan <sebake01@gmail.com>
2020-04-04 15:53:17 +0200

    TvSound

---
+++ GameJamSpring2020/Assets/Scripts/PlaySoundOnTriggerStay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnTriggerStay : MonoBehaviour
+{
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && !source.isPlaying)
+        {
+            source.PlayOneShot(source.clip);
+            Debug.Log("Play On Stay");
+        }
+    }
+}
