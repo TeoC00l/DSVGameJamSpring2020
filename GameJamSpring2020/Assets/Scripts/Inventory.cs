4cf04133c2daf879cf806ce217c285b50dd6a70b
disa <disaviolaelisabeth@gmail.com>
2020-04-04 14:08:04 +0200

    UI and pickup

--- GameJamSpring2020/Assets/Scripts/Inventory.cs
+++ GameJamSpring2020/Assets/Scripts/Inventory.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public static int collectedRolls = 0;
+    private static int noOfCollectibles = 8;
+    public int CollectedRolls = 0;
+
+    public static Inventory instance;
 
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(CollectedRolls >= noOfCollectibles)
+        {
+            //do win condition
+        }
     }
 }
