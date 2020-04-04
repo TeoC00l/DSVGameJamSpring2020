bce7aa9f6f045a9af43edb8a8d3614032a7e0fca
Bazzan <sebake01@gmail.com>
2020-04-04 19:27:14 +0200

    Tv with flickering lights

--- GameJamSpring2020/Assets/Scripts/PlaySoundOnTriggerStay.cs
+++ GameJamSpring2020/Assets/Scripts/PlaySoundOnTriggerStay.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlaySoundOnTriggerStay : MonoBehaviour
 {
     AudioSource source;
+   [SerializeField] Light[] lights;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,30 @@ public class PlaySoundOnTriggerStay : MonoBehaviour
     {
         if (other.CompareTag("Player") && !source.isPlaying)
         {
+
             source.PlayOneShot(source.clip);
+            StartCoroutine(flickeringLight());
             Debug.Log("Play On Stay");
+
         }
+    }
+
+
+    private IEnumerator flickeringLight()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            foreach (Light light in lights)
+            {
+                light.enabled = true;
+                yield return new WaitForSeconds(0.25f);
+            }
+            foreach (Light light in lights)
+            {
+                light.enabled = false;
+                yield return new WaitForSeconds(0.25f);
+            }
+        }
+        
     }
 }
