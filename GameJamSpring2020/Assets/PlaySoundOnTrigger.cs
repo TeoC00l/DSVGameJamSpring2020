8c7100a3da4a5fea72fd9c8688c2c0d2e518b60a
Bazzan <sebake01@gmail.com>
2020-04-04 15:44:09 +0200

    Sound and radio

---
+++ GameJamSpring2020/Assets/PlaySoundOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnTrigger : MonoBehaviour
+{
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            source.PlayOneShot(source.clip);
+        }
+    }
+
+}
