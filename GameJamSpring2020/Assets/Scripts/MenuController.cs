f1aadd2a82cd2033c69cb3c4af99ef2098dda450
disa <disaviolaelisabeth@gmail.com>
2020-04-05 13:09:14 +0200

    video player

--- GameJamSpring2020/Assets/Scripts/MenuController.cs
+++ GameJamSpring2020/Assets/Scripts/MenuController.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject MenuContent;
     public GameObject Controls;
+    public GameObject VideoScreen;
+    public VideoPlayer VideoPlayer;
+
+    private bool startedPlaying = false;
+    private bool played = false;
 
     private void OnEnable()
     {
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void Update()
+    {
+        if (startedPlaying && !VideoPlayer.isPlaying)
+        {
+            LoadScene("SampleScene");
+        }
+        if(Input.GetKeyDown(KeyCode.Return) && VideoPlayer.isPlaying)
+        {
+            LoadScene("SampleScene");
+        }
     }
 
     public void ShowControls()
@@ -25,6 +43,13 @@ public class MenuController : MonoBehaviour
             MenuContent.SetActive(false);
             Controls.SetActive(true);
         }
+    }
+
+    public void StartVideo()
+    {
+        VideoScreen.SetActive(true);
+        VideoPlayer.Play();
+        startedPlaying = true;
     }
 
     public void LoadScene(string sceneToLoad)
